68057422ee8c32bd4899fa82abe4130f8ec8b80f
ciara <g00402357@atu.ie>
2023-12-27 16:09:30 +0000

    Updated person service class

--- src/main/java/ie/atu/week11example/PersonRepository.java
+++ src/main/java/ie/atu/week11example/PersonRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //access to all crud operations by this
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Person findByEmployeeId(String employeeId);
 }
